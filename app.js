fe364807db2c1db44474275001fd0e8543ff7f2c
jbenhod <jbenhod@gmail.com>
2021-10-28 13:58:31 +0300

    Campground New & Create

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
+// app routes
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -27,6 +29,23 @@ app.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/show", { campground });
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// app listen
 app.listen(3000, () => {
   console.log("Serving on port 3000");
 });
