722e83f397bf981f582631ba985397fcca2aa377
JedBh <jbenhod@gmail.com>
2021-11-28 15:49:56 +0200

    reviews permissions

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,14 +6,16 @@ const { reviewSchema } = require("../schemas.js");
 const ExpressError = require("../utils/ExpressError");
 const Review = require("../models/review");
 const Campground = require("../models/campground");
-const { validateReview } = require("../middleware");
+const { validateReview, isLoggedIn } = require("../middleware");
 
 router.post(
   "/",
   validateReview,
+  isLoggedIn,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
