cc3c0b6c945f943f63ed5827787dd994f52804ac
JedBh <jbenhod@gmail.com>
2021-11-28 12:34:40 +0200

    starting authrization

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
-  // console.log("req.user:", req.user);
+  req.session.returnTo = req.originalUrl;
   if (!req.isAuthenticated()) {
     req.flash("error", "You must be signed in!");
     return res.redirect("/login");
