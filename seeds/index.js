61def99b4f197cffe5ceb77677d2972a47a6f94a
JedBh <jbenhod@gmail.com>
2021-12-05 14:38:30 +0200

    fixing our seeds

--- seeds/index.js
+++ seeds/index.js
@@ -27,10 +27,19 @@ const seedDB = async () => {
       author: "61a35c87477ee874fbc7937c",
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      image: "https://source.unsplash.com/collection/483251",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quo dolore fuga, laudantium amet iusto hic debitis similique itaque error praesentium, omnis commodi eum autem facere laborum doloribus architecto. Ad!",
       price: price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dwv3q539r/image/upload/v1638707142/yelpcamp/dtkjikuktujd52hqltez.jpg",
+          filename: "yelpcamp/dtkjikuktujd52hqltez",
+        },
+        {
+          url: "https://res.cloudinary.com/dwv3q539r/image/upload/v1638707141/yelpcamp/cv9rjwkjkkvytnqiqth4.jpg",
+          filename: "yelpcamp/cv9rjwkjkkvytnqiqth4",
+        },
+      ],
     });
     await camp.save();
   }
