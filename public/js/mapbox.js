9afdcc926df67c3951a5dc138810d53089298bcd
JedBh <jbenhod@gmail.com>
2021-12-23 01:16:35 +0200

    customizing map popups

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -3,9 +3,16 @@ objCampground = JSON.parse(campground);
 mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
-  center: objCampground.coordinates,
+  style: "mapbox://styles/mapbox/dark-v9", // style URL
+  center: objCampground.geometry.coordinates,
   zoom: 10, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(objCampground.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(objCampground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${objCampground.title}</h3><p>${objCampground.location}</p>`
+    )
+  )
+  .addTo(map);
